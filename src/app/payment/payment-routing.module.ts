8752c310a92586de353cc7aebccb1d84533df67c
RomanLesovoy <romadesire97@gmail.com>
2024-03-25 11:58:13 +0200

    change payment routes & added loader

--- src/app/payment/payment-routing.module.ts
+++ src/app/payment/payment-routing.module.ts
@@ -1,20 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DepositComponent } from './deposit/deposit.component';
-import { WithdrawComponent } from './withdraw/withdraw.component';
 
 const routes: Routes = [
   {
-    path: 'deposit',
+    path: '',
     component: DepositComponent,
   },
   {
-    path: 'withdraw',
-    component: WithdrawComponent,
-  },
-  {
     path: '**',
-    redirectTo: 'deposit'
+    redirectTo: ''
   }
 ];
 
